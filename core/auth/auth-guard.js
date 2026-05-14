6b215bb46cb1a2f3ce21c94ce37046442c464aa2
salah-hamed <salahhamed236@gmail.com>
2026-05-14 16:25:08 +0300

    Update auth-guard.js

--- core/auth/auth-guard.js
+++ core/auth/auth-guard.js
@@ -1,19 +1,10 @@
 import auth from "../firebase/firebase-auth.js";
-
-import {
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 export function protectPage() {
-
   onAuthStateChanged(auth, (user) => {
-
     if (!user) {
-
-      window.location.href = "/";
-
+      window.location.href = "/Familybusiness/";
     }
-
   });
-
 }
