ad928462268ad6514de8b669537f3f5ced6f1c22
salah-hamed <salahhamed236@gmail.com>
2026-05-14 16:44:56 +0300

    Update orders.js

--- templates/cleaning/orders.js
+++ templates/cleaning/orders.js
@@ -1,1 +1,29 @@
+import db from "../../core/firebase/firebase-db.js";
 
+import {
+  collection,
+  addDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export async function createOrder(orderData) {
+
+  try {
+
+    await addDoc(collection(db, "orders"), {
+      ...orderData,
+      createdAt: serverTimestamp()
+    });
+
+    return { success: true };
+
+  } catch(error) {
+
+    return {
+      success: false,
+      error: error.message
+    };
+
+  }
+
+}
