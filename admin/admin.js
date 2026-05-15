9a24ab4a671d5e435938afe27d3982b3c32b4eeb
salah-hamed <salahhamed236@gmail.com>
2026-05-15 17:50:47 +0300

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,83 @@
+import db from "../core/firebase/firebase-db.js";
 
+import {
+  collection,
+  getDocs,
+  doc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const usersContainer =
+  document.getElementById("usersContainer");
+
+loadUsers();
+
+async function loadUsers() {
+
+  const snapshot =
+    await getDocs(collection(db, "users"));
+
+  usersContainer.innerHTML = "";
+
+  snapshot.forEach((userDoc) => {
+
+    const data = userDoc.data();
+
+    const card = document.createElement("div");
+
+    card.style.border = "1px solid #ddd";
+    card.style.padding = "10px";
+    card.style.margin = "10px 0";
+
+    card.innerHTML = `
+
+      <p><b>${data.name}</b></p>
+      <p>${data.email}</p>
+      <p>Active: ${data.isActive}</p>
+
+      <select id="project-${data.uid}">
+        <option value="cleaning">cleaning</option>
+        <option value="coffee">coffee</option>
+        <option value="gas">gas</option>
+        <option value="services">services</option>
+        <option value="carwash">carwash</option>
+      </select>
+
+      <br><br>
+
+      <button id="activate-${data.uid}">
+        Activate
+      </button>
+
+    `;
+
+    usersContainer.appendChild(card);
+
+    setTimeout(() => {
+
+      document
+        .getElementById(`activate-${data.uid}`)
+        .addEventListener("click", async () => {
+
+          const selectedProject =
+            document.getElementById(`project-${data.uid}`).value;
+
+          await updateDoc(
+            doc(db, "users", data.uid),
+            {
+              isActive: true,
+              projectType: selectedProject
+            }
+          );
+
+          alert("Updated Successfully");
+
+          loadUsers();
+
+        });
+
+    }, 100);
+
+  });
+
+}
