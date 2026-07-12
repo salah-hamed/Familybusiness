835554f5a72c91109caabba9099389235248778e
salah-hamed <salahhamed236@gmail.com>
2026-07-12 14:48:59 +0300

    Update app.js

--- my-projects/app.js
+++ my-projects/app.js
@@ -7,7 +7,9 @@ import {
 
 import {
   doc,
-  getDoc
+  getDoc,
+  collection,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const container =
@@ -35,11 +37,70 @@ onAuthStateChanged(auth, async (user) => {
   const data = snap.data();
 
   container.innerHTML = `
-    <h2>مرحباً ${data.name}</h2>
+<h2>مرحباً ${data.name}</h2>
 
-    <p>حالة الاشتراك:
-      ${data.subscriptionStatus || "غير مفعل"}
-    </p>
-  `;
+<p>
+ترخيص المنصة:
+<b style="color:green;">
+${data.isActive ? "مفعل" : "غير مفعل"}
+</b>
+</p>
 
+<hr>
+
+<div id="projectsList">
+
+جاري تحميل المشاريع...
+
+</div>
+`;
+const projectsList =
+document.getElementById("projectsList");
+
+const templates =
+await getDocs(collection(db, "templates"));
+
+projectsList.innerHTML = "";
+
+templates.forEach((templateDoc) => {
+
+  const template = templateDoc.data();
+const templateId = templateDoc.id;
+
+  if (!template.active) return;
+
+  projectsList.innerHTML += `
+<div style="
+border:1px solid #ddd;
+padding:15px;
+margin:15px 0;
+border-radius:12px;
+">
+
+<h3>
+
+${template.icon}
+
+${template.title}
+
+</h3>
+
+<p>
+
+${template.description}
+
+</p>
+
+<button
+class="manageProjectBtn"
+data-project="${templateId}"
+>
+
+إدارة المشروع
+
+</button>
+</div>
+`;
+
+});
 });
