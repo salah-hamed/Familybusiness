b7a281f6c8b90e7b6c81191afd12953897547c77
salah-hamed <salahhamed236@gmail.com>
2026-07-13 13:41:41 +0300

    Update app.js

--- home/app.js
+++ home/app.js
@@ -1,1 +1,49 @@
+import db from "../core/firebase/firebase-db.js";
 
+import {
+collection,
+getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const projectsContainer =
+document.getElementById("projectsContainer");
+
+loadProjects();
+
+async function loadProjects(){
+
+const snapshot =
+await getDocs(collection(db,"templates"));
+
+projectsContainer.innerHTML="";
+
+snapshot.forEach((doc)=>{
+
+const project=doc.data();
+
+if(!project.active) return;
+
+projectsContainer.innerHTML += `
+
+<div class="project-card">
+
+<h3>
+
+${project.icon}
+${project.title}
+
+</h3>
+
+<p>
+
+${project.description}
+
+</p>
+
+</div>
+
+`;
+
+});
+
+}
