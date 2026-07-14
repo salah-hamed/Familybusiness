6406ffdb2d02470341ee7662740790b9ca6264c0
salah-hamed <salahhamed236@gmail.com>
2026-07-14 18:21:38 +0300

    Update app.js

--- home/app.js
+++ home/app.js
@@ -1,50 +1,22 @@
 import db from "../core/firebase/firebase-db.js";
 
 import {
-collection,
-getDocs
+  collection,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const projectsContainer =
 document.getElementById("projectsContainer");
-projectsContainer.innerHTML += `
 
-<div class="project-card">
+loadProjects();
 
-    <div class="project-icon">
+async function loadProjects(){
 
-        ${project.icon}
+    projectsContainer.innerHTML = "جاري تحميل المشاريع...";
 
-    </div>
+    const snapshot =
+    await getDocs(collection(db,"templates"));
 
-    <h3>
-
-        ${project.title}
-
-    </h3>
-
-    <p>
-
-        ${project.description}
-
-    </p>
-
-    <span class="project-status">
-
-        ✓ جاهز للعمل
-
-    </span>
-
-    <a href="#" class="project-btn">
-
-        شاهد المشروع
-
-    </a>
-
-</div>
-
-`;
-
-});
+    projectsContainer.innerHTML = "";
 
 }
