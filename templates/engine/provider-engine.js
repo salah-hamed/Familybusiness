8f8a8808f3e703b0f0a114dce1ddfd40b774c2cc
salah-hamed <salahhamed236@gmail.com>
2026-07-16 13:03:27 +0300

    Update provider-engine.js

--- templates/engine/provider-engine.js
+++ templates/engine/provider-engine.js
@@ -1,1 +1,51 @@
+import db from "../../core/firebase/firebase-db.js";
 
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export async function loadProvider(providerId) {
+
+  if (!providerId) {
+
+    return {
+      success: false,
+      error: "Provider ID not found"
+    };
+
+  }
+
+  const snap =
+    await getDoc(doc(db, "users", providerId));
+
+  if (!snap.exists()) {
+
+    return {
+      success: false,
+      error: "Provider not found"
+    };
+
+  }
+
+  const provider =
+    snap.data();
+
+  if (!provider.isActive) {
+
+    return {
+      success: false,
+      error: "Project not active"
+    };
+
+  }
+
+  return {
+
+    success: true,
+
+    provider
+
+  };
+
+}
